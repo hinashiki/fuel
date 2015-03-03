9b8baf122313a13727ea47868587e00e9ee1e606
hinashiki <aono@slash-one.com>
2015-03-03 16:07:56 +0900

    add variable $jqeury_version.

--- fuel/app/classes/controller/template.php
+++ fuel/app/classes/controller/template.php
@@ -19,6 +19,7 @@ class Controller_Template extends \Fuel\Core\Controller_Template {
 		$this->template->title = $this->_title;
 		$this->template->css = $this->_css;
 		$this->template->js = $this->_js;
+		$this->template->jquery_version = $this->_jquery_version;
 		$this->template->bread_crumb = \View::forge('elements/bread_crumb');
 		$this->template->bread_crumb->crumbs = $this->_crumbs;
 		return parent::after($response);
