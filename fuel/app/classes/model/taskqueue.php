853b3b86ef9d87fa3394b2adfa4c09f7ba852b2b
hinashiki <aono@slash-one.com>
2015-07-10 14:52:59 +0900

    Add $_observer to Model_TaskQueue for overwrap

--- fuel/app/classes/model/taskqueue.php
+++ fuel/app/classes/model/taskqueue.php
@@ -4,6 +4,23 @@
  */
 class Model_TaskQueue extends \Queue\Model_TaskQueue
 {
+
+	/**
+	 * @override
+	 */
+	protected static $_observers = array(
+		'Orm\\Observer_CreatedAt' => array(
+			'events' => array('before_insert'),
+			'mysql_timestamp' => true,
+			'property' => 'created_at',
+		),
+		'Orm\\Observer_UpdatedAt' => array(
+			'events' => array('before_save'),
+			'mysql_timestamp' => true,
+			'property' => 'updated_at',
+		),
+	);
+
 	/**
 	 * @override
 	 */
